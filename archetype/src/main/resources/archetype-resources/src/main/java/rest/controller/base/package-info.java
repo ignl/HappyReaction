40bf2650fa34938ac56d83c0fe2cb1c40a78e1ec
Ignas <keptavista@gmail.com>
2017-08-28 23:19:57 +0300

    Delete

--- archetype/src/main/resources/archetype-resources/src/main/java/rest/controller/base/package-info.java
+++
@@ -1,7 +0,0 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-/**
- * Package for Base parent classes used by REST endpoints implementations.
- */
-package ${package}.rest.controller.base;
